fc2a544cbf07d60bf470cc75d0115f1e282fc6c3
manda <mandy.farmer@gmail.com>
2017-06-13 23:05:16 -0400

    fix up with cow gif url

--- Main.elm
+++ Main.elm
@@ -108,7 +108,7 @@ getCowGif : Cmd EdocMsg
 getCowGif =
     let
         url =
-            "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy+cow"
+            "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=happy+cow&rating=g"
 
         request =
             Http.get url decodeGifUrl
