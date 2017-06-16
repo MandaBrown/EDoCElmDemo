dd3b224cf94a9ebf344985648e4e8dcee5344a00
manda <mandy.farmer@gmail.com>
2017-06-15 22:14:36 -0400

    drop in model update view

--- Main.elm
+++ Main.elm
@@ -6,3 +6,23 @@ import Html exposing (Html, div, text, h1)
 main : Html msg
 main =
     h1 [] [ text "Hello, World!" ]
+
+
+
+-- MODEL
+--type alias Model = { ... }
+--
+--
+-- UPDATE
+--type Msg = Reset | ...
+--update : Msg -> Model -> Model
+--update msg model =
+--  case msg of
+--    Reset -> ...
+--    ...
+--
+--
+-- VIEW
+--view : Model -> Html Msg
+--view model =
+--  ...
